64341b18f1d2a4a06578a3c6bd8cdd5888678444
MOHAMMEDBILAL-007 <mohammedbilalsharief99@gmail.com>
2025-06-25 10:58:05 +0530

    added a file

---
+++ p1.py
@@ -0,0 +1,6 @@
+#check thr number is even or not even
+input_number = int(input("enter the number : "))
+if input_number % 2 == 0:
+    print("number is even")
+else:
+    print("number is not even")
