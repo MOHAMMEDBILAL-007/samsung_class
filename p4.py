e66006c2008f4ee95063ce0b2deb38805f255e0e
MOHAMMEDBILAL-007 <mohammedbilalsharief99@gmail.com>
2025-06-25 11:43:00 +0530

    one more

---
+++ p4.py
@@ -0,0 +1,11 @@
+average_score = int(input("enter teh average score of the student : "))
+if average_score in range(0,70):
+    print("result is fail")
+elif average_score in range(70,85):
+    print("result is second class")
+elif average_score in range(85,96):
+    print("result is first class")
+elif average_score in range(96,101):
+    print("result is excellent")
+else:
+    print("invalid average score")
