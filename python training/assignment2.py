b3b7186853c166755d20cc06f79f69c2a67eadf5
MOHAMMEDBILAL-007 <mohammedbilalsharief99@gmail.com>
2025-06-26 11:45:28 +0530

    assignment work

---
+++ python training/assignment2.py
@@ -0,0 +1,12 @@
+# biggest digit in a number
+input_number =input("enter the number to fing the biggest digit : ")
+max_digit = int(input_number[0])
+for i in input_number:
+    if max_digit < int(i):
+        max_digit = int(i)
+print("maximum digit in the number is :",max_digit)
+#---------------------------------------------------------------------------------
+# 2nd smallest digit
+# input_number = input("enter the number :")
+# for i in 
+
