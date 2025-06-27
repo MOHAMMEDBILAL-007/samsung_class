74f5ac7dc3821773668f8f04efd1910c94a347ce
MOHAMMEDBILAL-007 <mohammedbilalsharief99@gmail.com>
2025-06-27 14:30:46 +0530

    day 6

--- python training/p5.py
+++ python training/p5.py
@@ -1,3 +1,13 @@
-a = int(input("entare the number to print its math table :"))
-for i in range(1,11):
-    print("%2d * %2d = %3d"%(a,i,a*i))
+# a = int(input("entare the number to print its math table :"))
+# for i in range(1,11):
+#     print("%2d * %2d = %3d"%(a,i,a*i))
+try:
+    print("hello")
+    print(2/0)
+    print("bye")
+except Exception as a:
+    print("an error occured")
+except ZeroDivisionError as e:
+    print("never divided by zero")
+except :
+    print('error')
